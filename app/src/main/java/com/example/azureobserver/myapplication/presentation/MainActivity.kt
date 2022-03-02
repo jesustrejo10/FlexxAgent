23aa9177c834250e59428cd70360e72c6d721342
Jesus <jesustrejo10@gmail.com>
2022-03-02 15:48:26 -0400

    adding logic for implement js in android

--- app/src/main/java/com/example/azureobserver/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/azureobserver/myapplication/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.azureobserver.R
 import com.example.azureobserver.myapplication.presentation.viewmodel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import org.liquidplayer.service.MicroService
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        executeMicroservice()
+
+
+    }
+
+    private fun executeMicroservice() {
+
+        //Callback  for the ready event
+        val readyListener = MicroService.EventListener {
+                service, _, _ -> service.emit("ping")
+        }
+
+        //Callback for pong event
+        val pongListener = MicroService.EventListener {
+                _, _, jsonObject ->
+            val message = jsonObject.getString("message")
+            runOnUiThread {  }
+        }
+
+        //These listener are from the Js file, when these events happen,
+        // A callback will be executed, readyListener and pongListener respectively
+        val startListener =
+            MicroService.ServiceStartListener{
+                    service ->
+                service.addEventListener("ready", readyListener)
+                service.addEventListener("pong", pongListener)
+            }
+        //Uri for the microservice
+        val uri = MicroService.Bundle(this, "example")
+        val service = MicroService(this, uri,
+            startListener)
+        service.start()
     }
 
     override fun onCreateView(
