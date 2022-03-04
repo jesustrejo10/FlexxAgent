198cbdb6f691938234a9347be72540701d56c175
Jesus <jesustrejo10@gmail.com>
2022-03-04 12:14:41 -0400

    adding TODO

--- app/src/main/java/com/example/azureobserver/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/azureobserver/myapplication/presentation/MainActivity.kt
@@ -43,7 +43,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun initButtons() {
         binding.initIotClient.setOnClickListener {
-
             initIotHubClient()
         }
     }
