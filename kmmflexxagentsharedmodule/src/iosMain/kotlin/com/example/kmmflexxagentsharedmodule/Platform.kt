f35dfd415a1182c3cb08ede5ecd2ee9c974ca755
Jesus <jesustrejo10@gmail.com>
2022-03-14 11:09:12 -0400

    modifiy gradle

--- kmmflexxagentsharedmodule/src/iosMain/kotlin/com/example/kmmflexxagentsharedmodule/Platform.kt
+++ kmmflexxagentsharedmodule/src/iosMain/kotlin/com/example/kmmflexxagentsharedmodule/Platform.kt
@@ -1,7 +1,7 @@
 package com.example.kmmflexxagentsharedmodule
 
-import platform.UIKit.UIDevice
+
 
 actual class Platform actual constructor() {
-    actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    actual val platform: String = "Hi i am the lord panzon"
 }
